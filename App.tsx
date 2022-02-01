99b8daf610b568bb3238ef2375496f59dc56d9fd
GILMARA_HP\gilma <gilmarapq@hotmail.com>
2022-01-31 21:59:04 -0500

    Structuring the project

--- App.tsx
+++ App.tsx
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <ImageBackground 
+      source={{ uri:"https://images.unsplash.com/photo-1643661250640-61a53ea78c39?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=686&q=80"}}
+      style={styles.image}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +22,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image:{
+    width: '100%',
+    height: '100%'
+  }
 });
