8eeff14d8c69b1a79d2aa60f5865f7a90cd48555
GILMARA_HP\gilma <gilmarapq@hotmail.com>
2022-03-17 07:45:27 -0400

    feat: finished creating Product Details

--- src/screens/shop/ProductDetail/index.tsx
+++ src/screens/shop/ProductDetail/index.tsx
@@ -1,13 +1,37 @@
-import React from 'react';
-import { Text , FlatList } from 'react-native';
+import { Text,View,Image} from 'react-native';
+import { useSelector, RootStateOrAny} from 'react-redux';
+import { NavigationStackProp } from 'react-navigation-stack'
 import { styles } from './styles';
 
-interface IProductDetail {
-    title: string;
+interface INavigationProps {
+    navigation: NavigationStackProp; 
 }
 
-const ProductDetail = (props :IProductDetail) => {
-    return <Text>PRODUCT DETAIL</Text>
+const ProductDetail = ({ navigation}:INavigationProps) =>{   
+    console.log(navigation, 'navigation on Products Details')
+    const prodId = navigation.getParam('prodId');
+    const productSelected = useSelector((state: RootStateOrAny) => state.products.availableProducts.find(
+            prod=>prod.id === prodId))
+   
+
+    return(
+            <View>
+                <Image source={{uri: productSelected.imageUrl}}
+                resizeMode='cover'
+                style={styles.imageContainer}/>
+                <View style={styles.detail}>
+                    <Text style={styles.title}>{productSelected.title}</Text>
+                    <Text 
+                        numberOfLines={3}
+                        style={styles.description}>{productSelected.description}
+                    </Text>
+                    <Text style={styles.price}>{productSelected.price}</Text>
+                </View>
+            </View>
+            
+            
+            )
+
 }
 
 export default ProductDetail;
--- src/screens/shop/ProductDetail/styles.ts
+++ src/screens/shop/ProductDetail/styles.ts
@@ -1,3 +1,35 @@
 import { StyleSheet } from 'react-native';
+import themes from './../../../global/styles/themes'
 
-export const styles = StyleSheet.create({});
+
+export const styles = StyleSheet.create({  
+    imageContainer:{
+        width: '100%',
+        height: '85%'
+    },
+   detail:{
+       justifyContent: 'center',
+       alignItems: 'center',
+       backgroundColor: themes.colors.primary,
+       height: '15%',
+       padding: 10
+   },
+   title:{
+    fontFamily: themes.fonts.MSATitle700Bold,
+    color: themes.colors.white,
+    fontSize: 20
+},
+description:{
+    fontFamily: themes.fonts.MSA600SemiBoldItalic,
+    color: themes.colors.white,
+    fontSize: 12,
+    textAlign: 'center'
+},
+price:{
+    fontFamily: themes.fonts.MSA600SemiBoldItalic,
+    color: themes.colors.white,
+    fontSize: 13,
+    textAlign: 'center'
+}
+    
+});
