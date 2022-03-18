859d84ac329467b60c05ea5846a3605f5a0e25be
GILMARA_HP\gilma <gilmarapq@hotmail.com>
2022-03-17 21:37:09 -0400

    feat: added Android and Ios button 'Add to Cart' on product Details

--- src/screens/shop/ProductDetail/styles.ts
+++ src/screens/shop/ProductDetail/styles.ts
@@ -3,33 +3,43 @@ import themes from './../../../global/styles/themes'
 
 
 export const styles = StyleSheet.create({  
+    mainContainer:{
+        height: '100%'
+    },
     imageContainer:{
         width: '100%',
-        height: '85%'
+        height: '80%'
     },
    detail:{
        justifyContent: 'center',
        alignItems: 'center',
-       backgroundColor: themes.colors.primary,
-       height: '15%',
-       padding: 10
+       backgroundColor: themes.colors.beige,
+       height: '20%',
+       paddingVertical: 25
+     
    },
    title:{
     fontFamily: themes.fonts.MSATitle700Bold,
-    color: themes.colors.white,
-    fontSize: 20
+    color: themes.colors.navy,
+    fontSize: 20,
+    marginTop: 10
 },
 description:{
     fontFamily: themes.fonts.MSA600SemiBoldItalic,
-    color: themes.colors.white,
+    color: themes.colors.primary,
     fontSize: 12,
-    textAlign: 'center'
+    textAlign: 'center',
+    paddingHorizontal: 10
 },
 price:{
     fontFamily: themes.fonts.MSA600SemiBoldItalic,
-    color: themes.colors.white,
+    color: themes.colors.navy,
     fontSize: 13,
-    textAlign: 'center'
-}
-    
+    textAlign: 'center',
+    marginTop: 5
+
+},
+button:{ 
+    marginTop:10,   
+},   
 });
