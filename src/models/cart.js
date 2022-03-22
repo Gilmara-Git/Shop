ff8aedd77375cfc4db0cd7cddd640fd6b1fabc4f
GILMARA_HP\gilma <gilmarapq@hotmail.com>
2022-03-22 18:43:04 -0400

    feat: created a CartItem model constructor

---
+++ src/models/cart.js
@@ -0,0 +1,10 @@
+class CartItem {
+    constructor(quantity, productPrice, produdtTitle, sum){
+        this.quantity = quantity;
+        this.productPrice = productPrice;
+        this.produdtTitle = produdtTitle;
+        this.sum = sum;
+    }
+}
+
+export default CartItem;
