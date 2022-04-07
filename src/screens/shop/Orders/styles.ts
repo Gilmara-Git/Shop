3befc22500c935617a2ca52a25cafa2f0724d39f
GILMARA_HP\gilma <gilmarapq@hotmail.com>
2022-04-06 21:07:14 -0400

    feat: added initial style to Order page

--- src/screens/shop/Orders/styles.ts
+++ src/screens/shop/Orders/styles.ts
@@ -1,3 +1,11 @@
 import { StyleSheet } from 'react-native';
+import themes from '../../../global/styles/themes';
 
-export const styles = StyleSheet.create({});
+export const styles = StyleSheet.create({
+    container:{
+        flex: 1
+    },
+    title:{
+        fontFamily: themes.fonts.MSATitle700Bold , 
+    fontSize: 55   }
+});
