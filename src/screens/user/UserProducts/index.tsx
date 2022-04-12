6b2e46e5d01e404e7e8e6d4b0b892a3b397a7749
GILMARA_HP\gilma <gilmarapq@hotmail.com>
2022-04-11 20:30:34 -0400

    feat: created screens usersProducts

--- src/screens/user/UserProducts/index.tsx
+++ src/screens/user/UserProducts/index.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
-import { Text , FlatList } from 'react-native';
+import { FlatList, Platform } from 'react-native';
+import ProductItem from '../../../components/ProductItem';
+import { RootStateOrAny, useSelector } from 'react-redux';
+import AndroidButton from '../../../components/AndroidButton';
+import IosButton from '../../../components/IosButton';
 import { styles } from './styles';
 
-interface IUserProducts {
-    title: string;
-}
-
-const UserProducts = (props :IUserProducts) => {
-    return <Text>{props.title}</Text>
-}
+const UserProducts = () => {
+    const userProducts = useSelector((state:RootStateOrAny) => state.products.userProducts);
+    console.log(userProducts); 
+    return <FlatList 
+            data={userProducts} 
+            keyExtractor={item =>item.id}
+            renderItem={(itemData)=>
+                    <ProductItem 
+                        title={itemData.item.title}
+                        price={itemData.item.price}
+                        imageUrl={itemData.item.imageUrl}
+                    >
+                        {Platform.OS === 'ios'? 
+                        (<IosButton title='Edit'/>) : 
+                        (<AndroidButton title='Delete'/>)}
+                    </ProductItem>
+                
+                }
+            />;
+} 
 
 export default UserProducts;
