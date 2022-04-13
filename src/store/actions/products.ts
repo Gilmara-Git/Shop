320938f1296562bc5e4288c7469f5771f5551e40
GILMARA_HP\gilma <gilmarapq@hotmail.com>
2022-04-12 21:58:36 -0400

    refactor: change if else on the Actions(product, cart and order) to switch case

--- src/store/actions/products.ts
+++ src/store/actions/products.ts
@@ -1,7 +1,14 @@
-export const UPDATEPRODUCT = 'UPDATEPRODUCT';
-type props = {
-    id: string;
+export const UPDATE_PRODUCT = "UPDATE_PRODUCT";
+export const DELETE_PRODUCT = "DELETE_PRODUCT";
+
+type Product = {
+  productId: string;
+};
+
+export function updateProduct({ productId }: Product) {
+    return { type: UPDATE_PRODUCT, pid: productId };
 }
-export const updateProduct = (id: props)=>{
-    return { type: UPDATEPRODUCT, pid: id }
+
+export function deleteProduct(productId: Product) {
+  return { type: DELETE_PRODUCT, pid: productId };
 }
