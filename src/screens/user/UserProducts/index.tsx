571c180e865e3c467bdfae304c74e131a7fcf67d
GILMARA_HP\gilma <gilmarapq@hotmail.com>
2022-04-12 21:29:27 -0400

    feat: implemented the user's ability to delete a product from userProducts and from availableProducts

--- src/screens/user/UserProducts/index.tsx
+++ src/screens/user/UserProducts/index.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
 import { FlatList, Platform } from 'react-native';
 import ProductItem from '../../../components/ProductItem';
-import { RootStateOrAny, useSelector } from 'react-redux';
+import { RootStateOrAny, useSelector, useDispatch } from 'react-redux';
 import AndroidButton from '../../../components/AndroidButton';
 import IosButton from '../../../components/IosButton';
-import { styles } from './styles';
+import HeaderButton from '../../../components/UI/HeaderButton';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import * as productActions from  '../../../store/actions/products';
 
 const UserProducts = () => {
     const userProducts = useSelector((state:RootStateOrAny) => state.products.userProducts);
-    console.log(userProducts); 
+    const dispatch = useDispatch();
     return <FlatList 
             data={userProducts} 
             keyExtractor={item =>item.id}
@@ -19,12 +20,57 @@ const UserProducts = () => {
                         imageUrl={itemData.item.imageUrl}
                     >
                         {Platform.OS === 'ios'? 
-                        (<IosButton title='Edit'/>) : 
-                        (<AndroidButton title='Delete'/>)}
+                        (<IosButton 
+                            title='Edit'
+                            onPress={()=>{console.log(itemData.item.id)}}
+                            />) : 
+                        (<AndroidButton 
+                            title='Edit'
+                            onPress={()=>{console.log('edit')}}
+                            />)}
+
+                        {Platform.OS === 'ios'? 
+                        (<IosButton 
+                            title='Delete'
+                            onPress={()=> {dispatch(productActions.deleteProduct(itemData.item.id))}}
+                            />) : 
+                        (<AndroidButton 
+                            title='Delete'
+                            onPress={()=>dispatch(productActions.deleteProduct(itemData.item.id))}
+                            
+                        />)}
+
                     </ProductItem>
                 
                 }
             />;
-} 
+};
+
+UserProducts.navigationOptions = (navData)=>{
+    return { 
+        headerTitle: 'Your Products',
+        headerLeft:()=>(   
+                    <HeaderButtons HeaderButtonComponent={HeaderButton}>
+                        <Item 
+                            title='Menu'
+                            iconName='ios-menu'
+                            onPress={()=>{navData.navigation.toggleDrawer()}}
+                            />
+                    </HeaderButtons>
+                ),
+
+        headerRight:()=>(
+                    <HeaderButtons HeaderButtonComponent={HeaderButton}>
+                        <Item title='Save'
+                              iconName='save'
+                              onPress={()=>{console.log('SAVE')}}                        
+                        />
+
+                    </HeaderButtons>
+        )
+
+    }
+
+}
 
 export default UserProducts;
