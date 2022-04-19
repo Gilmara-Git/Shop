5ab1746b3660d8eec0fc1c65958363f081dc707a
GILMARA_HP\gilma <gilmarapq@hotmail.com>
2022-04-18 21:10:44 -0400

    refactor: refactor userProduct screen

--- src/screens/user/UserProducts/index.tsx
+++ src/screens/user/UserProducts/index.tsx
@@ -1,82 +1,104 @@
-import { FlatList, Platform } from 'react-native';
-import ProductItem from '../../../components/ProductItem';
-import { RootStateOrAny, useSelector, useDispatch } from 'react-redux';
-import AndroidButton from '../../../components/AndroidButton';
-import IosButton from '../../../components/IosButton';
-import HeaderButton from '../../../components/UI/HeaderButton';
-import { HeaderButtons, Item } from 'react-navigation-header-buttons';
-import * as productActions from  '../../../store/actions/products';
-import  { NavigationStackProp } from 'react-navigation-stack';
+import { FlatList, Platform } from "react-native";
+import ProductItem from "../../../components/ProductItem";
+import { RootStateOrAny, useSelector, useDispatch } from "react-redux";
+import AndroidButton from "../../../components/AndroidButton";
+import IosButton from "../../../components/IosButton";
+import HeaderButton from "../../../components/UI/HeaderButton";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import * as productActions from "../../../store/actions/products";
+import { NavigationStackProp } from "react-navigation-stack";
 
-interface IEditProduct {
-    navigation : NavigationStackProp;
+interface IUserProduct {
+  navigation: NavigationStackProp;
 }
 
-const UserProducts = ({navigation}: IEditProduct) => {
-    console.log(navigation, 'navigation on EditProduct')
-    const userProducts = useSelector((state:RootStateOrAny) => state.products.userProducts);
-    const dispatch = useDispatch();
-    return <FlatList 
-            data={userProducts} 
-            keyExtractor={item =>item.id}
-            renderItem={(itemData)=>
-                    <ProductItem 
-                        title={itemData.item.title}
-                        price={itemData.item.price}
-                        imageUrl={itemData.item.imageUrl}
-                    >
-                        {Platform.OS === 'ios'? 
-                        (<IosButton 
-                            title='Edit'
-                            onPress={()=>{navigation.navigate('EditProduct')}}
-                            />) : 
-                        (<AndroidButton 
-                            title='Edit'
-                            onPress={()=>{navigation.navigate('EditProduct')}}
-                            />)}
+const UserProducts = ({ navigation }: IUserProduct) => {
 
-                        {Platform.OS === 'ios'? 
-                        (<IosButton 
-                            title='Delete'
-                            onPress={()=> {dispatch(productActions.deleteProduct(itemData.item.id))}}
-                            />) : 
-                        (<AndroidButton 
-                            title='Delete'
-                            onPress={()=>dispatch(productActions.deleteProduct(itemData.item.id))}
-                            
-                        />)}
+  const userProducts = useSelector(
+    (state: RootStateOrAny) => state.products.userProducts
+  );
+  const dispatch = useDispatch();
+  return (
+    <FlatList
+      data={userProducts}
+      keyExtractor={(item) => item.id}
+      renderItem={(itemData) => (
+        <ProductItem
+          title={itemData.item.title}
+          price={itemData.item.price}
+          imageUrl={itemData.item.imageUrl}
+        >
+          {Platform.OS === "ios" ? (
+            <IosButton
+              title="Edit"
+              onPress={() => {
+                navigation.navigate({
+                  routeName: "EditProduct",
+                  params: { prodId: itemData.item.id },
+                });
+              }}
+            />
+          ) : (
+            <AndroidButton
+              title="Edit"
+              onPress={() => {
+                navigation.navigate({
+                    routeName: "EditProduct",
+                    params: { prodId: itemData.item.id },
+                  });
+              }}
+            />
+          )}
 
-                    </ProductItem>
-                
-                }
-            />;
+          {Platform.OS === "ios" ? (
+            <IosButton
+              title="Delete"
+              onPress={() => {
+                dispatch(productActions.deleteProduct(itemData.item.id));
+              }}
+            />
+          ) : (
+            <AndroidButton
+              title="Delete"
+              onPress={() =>
+                dispatch(productActions.deleteProduct(itemData.item.id))
+              }
+            />
+          )}
+        </ProductItem>
+      )}
+    />
+  );
 };
 
-UserProducts.navigationOptions = (navData)=>{
-    return { 
-        headerTitle: 'Your Products',
-        headerLeft:()=>(   
-                    <HeaderButtons HeaderButtonComponent={HeaderButton}>
-                        <Item 
-                            title='Menu'
-                            iconName='ios-menu'
-                            onPress={()=>{navData.navigation.toggleDrawer()}}
-                            />
-                    </HeaderButtons>
-                ),
+UserProducts.navigationOptions = (navData : any) => {
+  return {
+    headerTitle: "Your Products",
+    headerLeft: () => (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Menu"
+          iconName="ios-menu"
+          onPress={() => {
+            navData.navigation.toggleDrawer();
+          }}
+        />
+      </HeaderButtons>
+    ),
 
-        headerRight:()=>(
-                    <HeaderButtons HeaderButtonComponent={HeaderButton}>
-                        <Item title='Save'
-                              iconName='save'
-                              onPress={()=>{console.log('SAVE')}}                        
-                        />
-
-                    </HeaderButtons>
-        )
-
-    }
-
-}
+    headerRight: () => (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Add"
+          iconName={Platform.OS ==='android'? 'add-circle-outline': 'ios-add-circle'}
+          onPress={() => {
+            navData.navigation.navigate('EditProduct'); 
+            // here we are not passing params as we will create a product
+          }}
+        />
+      </HeaderButtons>
+    ),
+  };
+};
 
 export default UserProducts;
