ab73e97c0bc465fff0f43ea27fef7adef2411d7a
GILMARA_HP\gilma <gilmarapq@hotmail.com>
2022-04-19 21:09:57 -0400

    feat:  created ACTIONS and REDUCER for CREATE_PRODUCT and UPDATE_PRODUCT

--- src/store/actions/products.ts
+++ src/store/actions/products.ts
@@ -1,14 +1,47 @@
 export const UPDATE_PRODUCT = "UPDATE_PRODUCT";
 export const DELETE_PRODUCT = "DELETE_PRODUCT";
+export const CREATE_PRODUCT = "CREATE_PRODUCT";
 
-type Product = {
+type ProductId = {
   productId: string;
 };
 
-export function updateProduct({ productId }: Product) {
-    return { type: UPDATE_PRODUCT, pid: productId };
+type Product = {
+  id: string;
+  title: string;
+  imageUrl: string;
+  description: string;
+  price?: number;
+};
+export function createProduct({
+  title,
+  imageUrl,
+  description,
+  price,
+}: Product) {
+  return {
+    type: CREATE_PRODUCT,
+    productData: {
+      title,
+      imageUrl,
+      description,
+      price,
+    },
+  };
 }
 
-export function deleteProduct(productId: Product) {
+export function updateProduct({ id, title, imageUrl, description }: Product) {
+  return {
+    type: UPDATE_PRODUCT,
+    pid: id,
+    productData: {
+      title,
+      imageUrl,
+      description,
+    }
+  };
+}
+
+export function deleteProduct(productId: ProductId) {
   return { type: DELETE_PRODUCT, pid: productId };
 }
--- src/store/reducers/products.ts
+++ src/store/reducers/products.ts
@@ -1,5 +1,8 @@
 import  PRODUCTS   from '../../data/dummy-data';
 import { DELETE_PRODUCT }  from '../actions/products';
+import { UPDATE_PRODUCT } from '../actions/products';
+import { CREATE_PRODUCT } from '../actions/products';
+import Product from '../../models/product';
 
 const initialState = {
     availableProducts: PRODUCTS,
@@ -7,7 +10,47 @@ const initialState = {
 }
 
 const productsReducer = (state = initialState, action: any)=>{
-    switch(action.type){
+    console.log(action.productData, 'action type no products reducer')
+
+    switch(action.productData){
+        case CREATE_PRODUCT:
+            const newProduct = new Product(
+                new Date().getMilliseconds().toString(), 
+                'u1',
+                action.productData.title,
+                action.productData.imageUrl,
+                action.productData.description,
+                action.productData.price);
+
+                return {
+                    ...state,
+                    userProducts: state.userProducts.concat(newProduct),
+                    availableProducts: state.availableProducts.concat(newProduct)
+                };
+
+        case UPDATE_PRODUCT:
+            const indexProd = state.userProducts.findIndex(prod=>prod.id === action.pid);
+            const upatedProduct =  new Product(
+                action.pid,
+                state.userProducts[indexProd].ownerId,
+                action.productData.title,
+                action.productData.imageUrl,
+                action.productData.description,
+                state.userProducts[indexProd].price
+                );
+                
+                let updatedUserProducts = [... state.userProducts ];
+                updatedUserProducts[indexProd] = upatedProduct;  
+                
+            const indexAvalProd = state.availableProducts.findIndex(prod=>prod.id === action.pid);
+            let updatedAvalProducts = [ ...state.availableProducts ];
+            updatedAvalProducts[indexAvalProd] = upatedProduct;
+
+            return {
+                ...state,
+                userProducts: updatedUserProducts,
+                availableProducts: updatedAvalProducts
+            }
         case DELETE_PRODUCT:
             const filteredProducts = state.userProducts.filter(prod=> prod.id !== action.pid);
             
@@ -16,8 +59,6 @@ const productsReducer = (state = initialState, action: any)=>{
                 userProducts:filteredProducts,
                 availableProducts: filteredProducts   
             };
-            
-
     }
 
     return state;
